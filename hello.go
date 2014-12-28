50d9ccba75b67a1e36c09fa58a00518f7a217738
root <matthew.steffen@gmail.com>
2014-12-28 05:32:34 -0500

    Added a bunch of ways to list the files in the current directory, to learn about a couple of go libraries

--- hello.go
+++ hello.go
@@ -30,35 +30,68 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "r.URL.Path[1:] = \"%s\"", r.URL.Path[1:])
 }
 
-var fake_links = Files{
-	[]Link{
-		Link{"N1", "L1"},
-		Link{"N2", "L2"},
-		Link{"N3", "L3"},
-	}}
+// Quick, ad-hoc test. I just kept this as reference for writing slice literals
+// TODO: figure out how to put this in a real test
+// var fake_links = Files{
+// 	[]Link{
+// 		Link{"N1", "L1"},
+// 		Link{"N2", "L2"},
+// 		Link{"N3", "L3"},
+// 	}}
+
+func ToStringSlice(words [][]byte) []string {
+	result := make([]string, len(words))
+	for i := 0; i < len(words); i++ {
+		result[i] = string(words[i])
+	}
+	return result
+}
+
+// Gets the contents of the current dir by fork/executing "ls"
+func PrintCurrentDirs1() (filenames []string, err error) {
+	cmd := exec.Command("ls")
+	stdout, e := cmd.StdoutPipe()
+	if e != nil {
+		return nil, e
+	}
+	if e := cmd.Start(); e != nil {
+		return nil, e
+	}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(stdout)
+	if e := cmd.Wait(); e != nil {
+		return nil, e
+	}
+	return ToStringSlice(bytes.Split(buf.Bytes(), []byte{'\n'})), nil
+}
+
+// Gets the contents of the current dir with os.Readdir(".") (presumably
+// implemented as a system call)
+func PrintCurrentDirs2() (filenames []string, err error) {
+	curdir, e := os.Open(".")
+	if e != nil {
+		return nil, e
+	}
+	result, e := curdir.Readdirnames(-1)
+	return result, e
+}
 
 func main() {
 	// http.HandleFunc("/", handler)
 	// http.ListenAndServe(":80", nil)
 
 	// Alternatively, os.Readdir(".")
-	cmd := exec.Command("ls")
-	stdout, e := cmd.StdoutPipe()
+	files, e := PrintCurrentDirs2()
 	if e != nil {
 		panic(e)
 	}
-	if e := cmd.Run(); e != nil {
-		panic(e)
-	}
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(stdout)
-	files := Split(buf.String(), "\n")
-	links = make([]Link, len(files))
-	for i := range len(files) {
-		links[i] = Link{files[i], files[i]}
+	links := make([]Link, len(files))
+	for i := 1; i < len(files); i++ {
+		links[i] = Link{string(files[i]), string(files[i])}
 	}
 
-	if t, e := template.ParseFiles("index.html"); e != nil {
+	t, e := template.ParseFiles("index.html")
+	if e != nil {
 		panic(e)
 	}
 
